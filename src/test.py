2e191b23a915b723e51ab085e241263092fd7f8e
Joe Joyce <joe@decafjoe.com>
2018-01-20 13:23:27 -0500

    Updates copyright year to 2018.

--- src/test.py
+++ src/test.py
@@ -3,7 +3,7 @@
 Tests for flake8-ownership.
 
 :author: Joe Joyce <joe@decafjoe.com>
-:copyright: Copyright (c) Joe Joyce and contributors, 2016-2017.
+:copyright: Copyright (c) Joe Joyce and contributors, 2016-2018.
 :license: BSD
 """
 import datetime

99a2495f613957b7a9de311638c8cef6b27e1ff0
Joe Joyce <joe@decafjoe.com>
2019-01-02 21:34:24 -0500

    Updates copyright year to 2019.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 Package configuration for flake8-ownership.
 
 :author: Joe Joyce <joe@decafjoe.com>
-:copyright: Copyright (c) Joe Joyce and contributors, 2016-2018.
+:copyright: Copyright (c) Joe Joyce and contributors, 2016-2019.
 :license: BSD
 """
 from setuptools import setup
--- src/flake8_ownership.py
+++ src/flake8_ownership.py
@@ -3,7 +3,7 @@
 Flake8 extension for checking author, copyright, and license.
 
 :author: Joe Joyce <joe@decafjoe.com>
-:copyright: Copyright (c) Joe Joyce and contributors, 2016-2018.
+:copyright: Copyright (c) Joe Joyce and contributors, 2016-2019.
 :license: BSD
 """
 import datetime
